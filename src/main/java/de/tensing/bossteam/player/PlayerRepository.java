3da3d699ca236ea5d821073679f179046933b9be
Jannis <jannis@holleyn.de>
2021-10-10 22:49:28 +0200

    added functions that are meant by PLAYER_WRITE

--- src/main/java/de/tensing/bossteam/player/PlayerRepository.java
+++ src/main/java/de/tensing/bossteam/player/PlayerRepository.java
@@ -2,5 +2,5 @@ package de.tensing.bossteam.player;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PlayerRepository extends JpaRepository<Player, Integer> {
+public interface PlayerRepository extends JpaRepository<PlayerDTO, Integer> {
 }
