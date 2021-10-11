bb4d72bd7bcff55b6bd18449ab159a768fac6ed8
Jannis <jannis@holleyn.de>
2021-10-11 18:54:59 +0200

    added functions that are meant by PLAYER_WRITE and added user roles

--- src/main/java/de/tensing/bossteam/security/ApplicationSecurityConfig.java
+++ src/main/java/de/tensing/bossteam/security/ApplicationSecurityConfig.java
@@ -12,8 +12,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-import static de.tensing.bossteam.security.ApplicationUserRole.ADMIN;
-import static de.tensing.bossteam.security.ApplicationUserRole.BOSSTEAMER;
+import static de.tensing.bossteam.security.ApplicationUserRole.*;
 
 @Configuration
 @EnableWebSecurity
@@ -26,8 +25,6 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Autowired
-
-
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
@@ -38,21 +35,50 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .httpBasic();
     }
+    private UserDetails createNewUser(String username, String password, ApplicationUserRole role) {
+        return User.builder()
+                .username(username)
+                .password(passwordEncoder.encode(password))
+                .roles(role.name())
+                .build();
+    }
 
     @Override
     @Bean
     protected UserDetailsService userDetailsService() {
-        UserDetails bossTeamUser = User.builder()
-                .username("bossteam")
-                .password(passwordEncoder.encode("bittekeinbit3"))
-                .roles(BOSSTEAMER.name())
-                .build();
+        UserDetails bossTeamUser = createNewUser("bossteam", "bittekeinbit3", BOSSTEAMER);
+        UserDetails jannismfUser = createNewUser("jannismf", "s0nnenschule", ADMIN);
 
-        UserDetails jannismfUser = User.builder()
-                .username("jannismf")
-                .password(passwordEncoder.encode("(jan5mnis)"))
-                .roles(ADMIN.name())
-                .build();
+        UserDetails p1User = createNewUser("p1", "rennen", P1);
+        UserDetails p2User = createNewUser("p2", "laib", P2);
+        UserDetails p3User = createNewUser("p3", "abendessen", P3);
+        UserDetails p4User = createNewUser("p4", "schuppen", P4);
+        UserDetails p5User = createNewUser("p5", "lippe", P5);
+        UserDetails p6User = createNewUser("p6", "eidechsen", P6);
+        UserDetails p7User = createNewUser("p7", "schiff", P7);
+        UserDetails p8User = createNewUser("p8", "alarm", P8);
+        UserDetails p9User = createNewUser("p9", "vorschlag", P9);
+        UserDetails p10User = createNewUser("p10", "ekel", P10);
+        UserDetails p11User = createNewUser("p11", "handel", P11);
+        UserDetails p12User = createNewUser("p12", "streichholz", P12);
+        UserDetails p13User = createNewUser("p13", "meisterschaft", P13);
+        UserDetails p14User = createNewUser("p14", "pferde", P14);
+        UserDetails p15User = createNewUser("p15", "stretch", P15);
+        UserDetails p16User = createNewUser("p16", "paket", P16);
+        UserDetails p17User = createNewUser("p17", "vers", P17);
+        UserDetails p18User = createNewUser("p18", "kohl", P18);
+        UserDetails p19User = createNewUser("p19", "steuer", P19);
+        UserDetails p20User = createNewUser("p20", "ehe", P20);
+        UserDetails p21User = createNewUser("p21", "termin", P21);
+        UserDetails p22User = createNewUser("p22", "menge", P22);
+        UserDetails p23User = createNewUser("p23", "eimer", P23);
+        UserDetails p24User = createNewUser("p24", "hammer", P24);
+        UserDetails p25User = createNewUser("p25", "knie", P25);
+        UserDetails p26User = createNewUser("p26", "wurzel", P26);
+        UserDetails p27User = createNewUser("p27", "bewegung", P27);
+        UserDetails p28User = createNewUser("p28", "sprache", P28);
+        UserDetails p29User = createNewUser("p29", "tante", P29);
+        UserDetails p30User = createNewUser("p30", "wissenschaft", P30);
 
         return new InMemoryUserDetailsManager(
                 bossTeamUser,
--- src/main/java/de/tensing/bossteam/security/ApplicationUserRole.java
+++ src/main/java/de/tensing/bossteam/security/ApplicationUserRole.java
@@ -16,7 +16,37 @@ public enum ApplicationUserRole {
             PLAYER_WRITE,
             GAME_START,
             GAME_RESET
-    ));
+    )),
+    P1(Sets.newHashSet(P1_PLAY)),
+    P2(Sets.newHashSet(P2_PLAY)),
+    P3(Sets.newHashSet(P3_PLAY)),
+    P4(Sets.newHashSet(P4_PLAY)),
+    P5(Sets.newHashSet(P5_PLAY)),
+    P6(Sets.newHashSet(P6_PLAY)),
+    P7(Sets.newHashSet(P7_PLAY)),
+    P8(Sets.newHashSet(P8_PLAY)),
+    P9(Sets.newHashSet(P9_PLAY)),
+    P10(Sets.newHashSet(P10_PLAY)),
+    P11(Sets.newHashSet(P11_PLAY)),
+    P12(Sets.newHashSet(P12_PLAY)),
+    P13(Sets.newHashSet(P13_PLAY)),
+    P14(Sets.newHashSet(P14_PLAY)),
+    P15(Sets.newHashSet(P15_PLAY)),
+    P16(Sets.newHashSet(P16_PLAY)),
+    P17(Sets.newHashSet(P17_PLAY)),
+    P18(Sets.newHashSet(P18_PLAY)),
+    P19(Sets.newHashSet(P19_PLAY)),
+    P20(Sets.newHashSet(P20_PLAY)),
+    P21(Sets.newHashSet(P21_PLAY)),
+    P22(Sets.newHashSet(P22_PLAY)),
+    P23(Sets.newHashSet(P23_PLAY)),
+    P24(Sets.newHashSet(P24_PLAY)),
+    P25(Sets.newHashSet(P25_PLAY)),
+    P26(Sets.newHashSet(P26_PLAY)),
+    P27(Sets.newHashSet(P27_PLAY)),
+    P28(Sets.newHashSet(P28_PLAY)),
+    P29(Sets.newHashSet(P29_PLAY)),
+    P30(Sets.newHashSet(P30_PLAY));
 
     private final Set<ApplicationUserPermission> permissions;
 
