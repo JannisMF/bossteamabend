cd21e2f0ccbba07268eba668e0287f54d14ac750
Jannis <jannis@holleyn.de>
2021-10-15 22:43:30 +0200

    cleanup

--- src/main/java/de/tensing/bossteam/repositories/PlayerRepository.java
+++
@@ -1,9 +0,0 @@
-package de.tensing.bossteam.repositories;
-
-import de.tensing.bossteam.repositories.entities.PlayerEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PlayerRepository extends CrudRepository<PlayerEntity, Integer> {
-
-    public PlayerEntity getPlayerById();
-}
--- src/main/java/de/tensing/bossteam/services/PlayerDetailsService.java
+++
@@ -1,8 +0,0 @@
-package de.tensing.bossteam.services;
-
-import javassist.NotFoundException;
-
-public interface PlayerDetailsService {
-
-    public PlayerDetails loadPlayerById(Integer id) throws NotFoundException;
-}
