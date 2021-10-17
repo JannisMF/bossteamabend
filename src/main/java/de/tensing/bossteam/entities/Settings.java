099e5ebd7ed347e6929c9c888c7b34d4d5672d24
Jannis <jannis@holleyn.de>
2021-10-18 00:22:53 +0200

    Working now. Testing with 150 MAX_PROGRESS

--- src/main/java/de/tensing/bossteam/entities/Settings.java
+++ src/main/java/de/tensing/bossteam/entities/Settings.java
@@ -1,11 +1,5 @@
 package de.tensing.bossteam.entities;
 
-import de.tensing.bossteam.entities.Player;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.IntStream;
-
 public final class Settings {
 
     public static final int NUMBER_OF_PLAYERS = 30;
@@ -13,5 +7,5 @@ public final class Settings {
     public static final int START_FOOD = 10;
     public static final int START_ARMOR = 0;
     public static final int START_PROGRESS = 0;
-    public static final int MAX_PROGRESS = 100;
+    public static final int MAX_PROGRESS = 150;
 }
